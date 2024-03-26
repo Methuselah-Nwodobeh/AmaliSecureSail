56a70d610589ae187a8c20f682936ba4dffaf040
Methuselah-Nwodobeh <methuselah.nwodobeh@amalitech.com>
2024-03-26 11:00:36 +0000

    Initial commit

---
+++ src/main/java/com/amalitech/securesail/amalisecuresail/AmaliSecureSailApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.securesail.amalisecuresail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaliSecureSailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmaliSecureSailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amalitech/securesail/amalisecuresail/AmaliSecureSailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.securesail.amalisecuresail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmaliSecureSailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
